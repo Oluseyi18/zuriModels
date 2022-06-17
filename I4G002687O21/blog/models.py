033c252fb96c31ffe3a353502deb706866f980e5
Ajibade Oluwaseyi <seyeezi18@gmail.com>
2022-06-17 23:14:26 +0100

    Zuri-Models Task

---
+++ I4G002687O21/blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(User)
+    created_date = models.DateTimeField()
+    published_date = models.DateTimeField()
